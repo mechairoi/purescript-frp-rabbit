f49392557299532a136415c37eee27ddb1b3455d
mechairoi <ttsujikawa@gmail.com>
2015-04-24 02:11:46 +0900

    Event based on ContT

--- src/FRP/Rabbit/Event.purs
+++ src/FRP/Rabbit/Event.purs
@@ -8,60 +8,52 @@ module FRP.Rabbit.Event
 import Control.Monad.Eff
 import Control.Monad.Eff.Ref
 import Data.Monoid
-import Data.Profunctor (lmap)
 import Data.Traversable (sequence)
 import Data.Foldable (sequence_)
+import Control.Monad.Cont.Trans
+import Control.Monad.Trans
 
 import FRP.Rabbit.Internal.Util
 
-data Mapped m a b = Mapped (a -> b) (m a)
-foreign import data MappedForeign :: (* -> *) -> * -> *
+data Event e a= Event (ContT (Eff (ref :: Ref | e) Unit) (Eff (ref :: Ref | e)) a)
 
-mapped :: forall m a b. MappedForeign m b -> Mapped m a b
-mapped = unsafeCoerce
-mappedForeign :: forall m a b. Mapped m a b -> MappedForeign m b
-mappedForeign = unsafeCoerce
-
-data Event e a = PureEvent (RefVal [ RefVal (SinkI e a) ])
-               | AppendedEvent (Event e a) (Event e a)
-               | MappedEvent (MappedForeign (Event e) a)
-
-newEventWithSource :: forall e a. WithRef e { event :: Event e a, source :: Sink e a }
-newEventWithSource = do
-  cbRefsRef <- newRef []
-  let event = PureEvent cbRefsRef
-  let source = \a -> do
-        cbRefs <- readRef cbRefsRef
-        afters <- sequence $ readRef >>> (>>= ($ a)) <$> cbRefs
-        sequence_ afters
-  return { event: event, source: source }
-
+-- Sink is after callback
+-- SinkI returns after callback
+-- runContT returns unsink(unsubscriber/unregisterer)
+-- sinkE returns unsink
 sinkE :: forall e a. Sink e a -> Event e a -> WithRef e (WithRef e Unit)
 sinkE snk = sinkEI \a -> return $ snk a
 
+-- sinkEI returns unsink
 sinkEI :: forall e a. SinkI e a -> Event e a -> WithRef e (WithRef e Unit)
-sinkEI snk (PureEvent snksRef) = do
-  snkRef <- newRef snk
-  modifyRef snksRef (snkRef :)
-  return $ do
-    modifyRef snksRef $ removeOnce (snkRef ==)
-    writeRef snkRef $ const $ return $ return unit
-sinkEI snk (AppendedEvent ma mb) = do
-  unregA <- sinkEI snk ma
-  unregB <- sinkEI snk mb
-  return $ do
-    unregA
-    unregB
-sinkEI snk (MappedEvent e) = sink snk $ mapped e
-  where
-    sink :: forall e a b. SinkI e b -> Mapped (Event e) a b -> WithRef e (WithRef e Unit)
-    sink snk (Mapped f ma) = sinkEI (lmap f snk) ma
+sinkEI snk (Event cont) = runContT cont snk
+
+newEventWithSource :: forall e a. WithRef e { event :: Event e a, source :: Sink e a }
+newEventWithSource = do
+  snkRefsRef <- newRef []
+  let event = Event $ ContT $ \snk -> do
+        snkRef <- newRef snk
+        modifyRef snkRefsRef (snkRef :)
+        return $ do -- unsink
+          modifyRef snkRefsRef $ removeOnce (snkRef ==)
+          writeRef snkRef $ const $ return $ return unit
+  let source = \a -> do
+        snkRefs <- readRef snkRefsRef
+        afters <- sequence $ readRef >>> (>>= ($ a)) <$> snkRefs
+        sequence_ afters
+  return { event: event, source: source }
 
 instance monoidEvent :: Monoid (Event e a) where
-  mempty = PureEvent $ unsafePerformEff $ newRef []
+  mempty = Event $ ContT $ \_ -> return $ return unit
 
 instance functorEvent :: Functor (Event e) where
-  (<$>) f ma = MappedEvent $ mappedForeign $ Mapped f ma
+  (<$>) f (Event ca) = Event $ ContT $ \g -> do
+    runContT ca (f >>> g)
 
 instance semigroupEvent :: Semigroup (Event e a) where
-  (<>) ma mb = AppendedEvent ma mb
+  (<>) (Event ca) (Event cb) = Event $ ContT $ \f -> do
+    unsnkA <- runContT ca f
+    unsnkB <- runContT cb f
+    return do
+      unsnkA
+      unsnkB
